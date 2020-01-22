e92cafe1e68bfb206a025e4ca649cea881179182
uVe10jaVa <uve.java@gmail.com>
2020-01-22 13:37:42 +0100

    title

---
+++ holamundo.c
@@ -0,0 +1,15 @@
+/*incluimos la librería*/
+#include <stdio.h>
+
+/*función principal - entre paréntesis, los parámetros que retorna -
+	int argc - retorna el Ok, el resultado de la ejecución del programa, que en c es un nº entero.
+	char *argv - es un array con el nº de elementos que tiene.
+	
+	printf es la función a ejecutar, con los argumentos que necesita entre paréntesis.
+*/
+int main (int argc, char *argv) {
+	printf("Hola mundo");
+	}
+
+/* el mismo ejemplo de arriba. otra manera de hacerlo es:
+void main(){} */
